04544e26a432ebe5bd262ce4789aea77e306c650
John Ehab <johnehab.m@gmail.com>
2026-06-02 12:08:23 +0300

    add output standalone to next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // ده الجزء اللي كان ناقص!
   webpack: (config, { dev, isServer }) => {
     if (!dev) {
       config.cache = false; // تعطيل الكاش في وضع الإنتاج
